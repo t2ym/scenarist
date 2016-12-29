506e968058a2d29e246c6caf9355002064846d23
Tetsuya Mori <t2y3141592@gmail.com>
2016-12-29 18:18:46 +0900

    [test] Rebuild test suites with skipAfterFailure tests

--- test/es5/error.js
+++ test/es5/error.js
@@ -1,11 +1,11 @@
 'use strict';
 // global test classes
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -13,7 +13,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var ErrorSuite = function (_Suite) {
+((typeof window === 'undefined' ? 'undefined' : _typeof(window)) === 'object' ? window : global).ErrorSuite = function (_Suite) {
   _inherits(ErrorSuite, _Suite);
 
   function ErrorSuite() {
@@ -60,7 +60,6 @@ var ErrorSuite = function (_Suite) {
 
   return ErrorSuite;
 }(Suite);
-
 {
   (function () {
     // error scope
@@ -270,6 +269,211 @@ var ErrorSuite = function (_Suite) {
                 }
               };
             }, /Suite[.]error:generateClass mixin InexistentMixin2 does not exist/);
+          });
+        });
+      });
+
+      (typeof suite === 'function' ? suite : describe)('Scenario test', function () {
+        (typeof suite === 'function' ? suite : describe)('skipAfterFailure test', function () {
+          (typeof test === 'function' ? test : it)('skipAfterFailure', function () {
+            error.test = t = function (_Suite2) {
+              _inherits(SkipAfterFailureSuite, _Suite2);
+
+              function SkipAfterFailureSuite() {
+                _classCallCheck(this, SkipAfterFailureSuite);
+
+                return _possibleConstructorReturn(this, (SkipAfterFailureSuite.__proto__ || Object.getPrototypeOf(SkipAfterFailureSuite)).apply(this, arguments));
+              }
+
+              _createClass(SkipAfterFailureSuite, [{
+                key: 'setup',
+                value: function setup() {
+                  return regeneratorRuntime.async(function setup$(_context3) {
+                    while (1) {
+                      switch (_context3.prev = _context3.next) {
+                        case 0:
+                          _context3.next = 2;
+                          return regeneratorRuntime.awrap(_get(SkipAfterFailureSuite.prototype.__proto__ || Object.getPrototypeOf(SkipAfterFailureSuite.prototype), 'setup', this).call(this));
+
+                        case 2:
+                          this.__failed = true;
+
+                        case 3:
+                        case 'end':
+                          return _context3.stop();
+                      }
+                    }
+                  }, null, this);
+                }
+              }], [{
+                key: 'skipAfterFailure',
+                get: function get() {
+                  return true;
+                }
+              }]);
+
+              return SkipAfterFailureSuite;
+            }(Suite);
+            error.test = function (base) {
+              return function (_base6) {
+                _inherits(SkippedTest, _base6);
+
+                function SkippedTest() {
+                  _classCallCheck(this, SkippedTest);
+
+                  return _possibleConstructorReturn(this, (SkippedTest.__proto__ || Object.getPrototypeOf(SkippedTest)).apply(this, arguments));
+                }
+
+                _createClass(SkippedTest, [{
+                  key: 'operation',
+                  value: function operation() {
+                    return regeneratorRuntime.async(function operation$(_context4) {
+                      while (1) {
+                        switch (_context4.prev = _context4.next) {
+                          case 0:
+                          case 'end':
+                            return _context4.stop();
+                        }
+                      }
+                    }, null, this);
+                  }
+                }, {
+                  key: 'checkpoint',
+                  value: function checkpoint() {
+                    return regeneratorRuntime.async(function checkpoint$(_context5) {
+                      while (1) {
+                        switch (_context5.prev = _context5.next) {
+                          case 0:
+                            assert.isOk(false, 'SkippedTest must be skipped');
+
+                          case 1:
+                          case 'end':
+                            return _context5.stop();
+                        }
+                      }
+                    }, null, this);
+                  }
+                }]);
+
+                return SkippedTest;
+              }(base);
+            };
+            error.test = {
+              SkipAfterFailureSuite: {
+                SkippedTest: 'SkipAfterFailureTest'
+              }
+            };
+            console.log(error.leafClasses);
+            new error.leafClasses.SkipAfterFailureTest().run();
+          });
+
+          (typeof test === 'function' ? test : it)('skipAfterFailure with iteration', function () {
+            error.test = t = function (_Suite3) {
+              _inherits(SkipAfterFailureSuite2, _Suite3);
+
+              function SkipAfterFailureSuite2() {
+                _classCallCheck(this, SkipAfterFailureSuite2);
+
+                return _possibleConstructorReturn(this, (SkipAfterFailureSuite2.__proto__ || Object.getPrototypeOf(SkipAfterFailureSuite2)).apply(this, arguments));
+              }
+
+              _createClass(SkipAfterFailureSuite2, [{
+                key: 'setup',
+                value: function setup() {
+                  return regeneratorRuntime.async(function setup$(_context6) {
+                    while (1) {
+                      switch (_context6.prev = _context6.next) {
+                        case 0:
+                          _context6.next = 2;
+                          return regeneratorRuntime.awrap(_get(SkipAfterFailureSuite2.prototype.__proto__ || Object.getPrototypeOf(SkipAfterFailureSuite2.prototype), 'setup', this).call(this));
+
+                        case 2:
+                          this.__failed = true;
+
+                        case 3:
+                        case 'end':
+                          return _context6.stop();
+                      }
+                    }
+                  }, null, this);
+                }
+              }], [{
+                key: 'skipAfterFailure',
+                get: function get() {
+                  return true;
+                }
+              }]);
+
+              return SkipAfterFailureSuite2;
+            }(Suite);
+            error.test = function (base) {
+              return function (_base7) {
+                _inherits(SkippedTest2, _base7);
+
+                function SkippedTest2() {
+                  _classCallCheck(this, SkippedTest2);
+
+                  return _possibleConstructorReturn(this, (SkippedTest2.__proto__ || Object.getPrototypeOf(SkippedTest2)).apply(this, arguments));
+                }
+
+                _createClass(SkippedTest2, [{
+                  key: 'iteration',
+                  value: regeneratorRuntime.mark(function iteration() {
+                    return regeneratorRuntime.wrap(function iteration$(_context7) {
+                      while (1) {
+                        switch (_context7.prev = _context7.next) {
+                          case 0:
+                            return _context7.delegateYield([1, 2, 3], 't0', 1);
+
+                          case 1:
+                          case 'end':
+                            return _context7.stop();
+                        }
+                      }
+                    }, iteration, this);
+                  })
+                }, {
+                  key: 'operation',
+                  value: function operation(parameters) {
+                    return regeneratorRuntime.async(function operation$(_context8) {
+                      while (1) {
+                        switch (_context8.prev = _context8.next) {
+                          case 0:
+                          case 'end':
+                            return _context8.stop();
+                        }
+                      }
+                    }, null, this);
+                  }
+                }, {
+                  key: 'checkpoint',
+                  value: function checkpoint(parameters) {
+                    return regeneratorRuntime.async(function checkpoint$(_context9) {
+                      while (1) {
+                        switch (_context9.prev = _context9.next) {
+                          case 0:
+                            assert.isOk(false, 'SkippedTest2 must be skipped');
+
+                          case 1:
+                          case 'end':
+                            return _context9.stop();
+                        }
+                      }
+                    }, null, this);
+                  }
+                }]);
+
+                return SkippedTest2;
+              }(base);
+            };
+            error.test = {
+              SkipAfterFailureSuite2: {
+                SkippedTest2: 'SkipAfterFailureTest2'
+              },
+              ErrorSuite: 'ErrorSuiteAlias'
+            };
+            console.log(error.branchScenarios);
+            new error.leafClasses.SkipAfterFailureTest2().run();
           });
         });
       });
