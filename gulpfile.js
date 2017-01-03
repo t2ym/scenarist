67ec9981346f8a5f7ee93a471e56bc09bb92ffb2
Tetsuya Mori <t2y3141592@gmail.com>
2017-01-03 15:19:16 +0900

    [demo] Add a demo with nano-calc

--- gulpfile.js
+++ gulpfile.js
@@ -135,6 +135,41 @@ gulp.task('build:test', () => {
     .pipe(gulp.dest('test/min'));
 });
 
+gulp.task('build:demo', () => {
+  return gulp.src([ 'demo/src/*.js', 'demo/src/*.html' ])
+    .pipe(replace('//require(\'babel-polyfill\')', 'require(\'babel-polyfill\')'))
+    .pipe(replace('<!-- <script src="../../node_modules/babel-polyfill/browser.js"></script> -->',
+      '<script src="../../node_modules/babel-polyfill/browser.js"></script>'))
+    .pipe(replace('/Suite.js', '/Suite.min.js'))
+    .pipe(gulpif('*.js', babel({
+      "presets": [ /*'es2015'*/ ],
+      "plugins": [
+        'check-es2015-constants',
+        'transform-es2015-arrow-functions',
+        'transform-es2015-block-scoped-functions',
+        'transform-es2015-block-scoping',
+        'transform-es2015-classes',
+        'transform-es2015-computed-properties',
+        'transform-es2015-destructuring',
+        'transform-es2015-duplicate-keys',
+        'transform-es2015-for-of',
+        'transform-es2015-function-name',
+        'transform-es2015-literals',
+        'transform-es2015-modules-commonjs',
+        'transform-es2015-object-super',
+        'transform-es2015-parameters',
+        'transform-es2015-shorthand-properties',
+        'transform-es2015-spread',
+        'transform-es2015-sticky-regex',
+        'transform-es2015-template-literals',
+        'transform-es2015-typeof-symbol',
+        'transform-es2015-unicode-regex',
+        'transform-regenerator'
+      ]
+    })))
+    .pipe(gulp.dest('demo/min'));
+});
+
 gulp.task('default', (done) => {
   runSequence('umd', 'es5', done);
 });
