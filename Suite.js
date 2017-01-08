bcaa945d99665df5a8b75389fdfda7c134f6ea9b
Tetsuya Mori <t2y3141592@gmail.com>
2017-01-08 21:13:07 +0900

    Rebuild with more coverage by return this.skip()

--- Suite.js
+++ Suite.js
@@ -448,7 +448,7 @@ class Suite {
                         (typeof parameters.name === 'function' ? parameters.name(parameters) : parameters.name)
                         : step.name, overrideToString(async function() {
                     if (self.constructor.skipAfterFailure && self.__failed) {
-                      this.skip();
+                      return this.skip();
                     }
                     else {
                       self.__failed = true;
@@ -467,7 +467,7 @@ class Suite {
             else {
               (typeof test === 'function' ? test : it)(step.name, overrideToString(async function() {
                 if (self.constructor.skipAfterFailure && self.__failed) {
-                  this.skip();
+                  return this.skip();
                 }
                 else {
                   self.__failed = true;
