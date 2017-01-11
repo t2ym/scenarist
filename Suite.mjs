1c4f5fb706d636cd963ddf433474eacc05eb5b75
Tetsuya Mori <t2y3141592@gmail.com>
2017-01-11 14:27:04 +0900

    Fix #25. Add exception handler method

--- Suite.mjs
+++ Suite.mjs
@@ -430,6 +430,7 @@ class Suite {
     else {
       return new Promise((resolve, reject) => {
         let exception;
+        let checkExceptionHandler = (reject, exception) => typeof self.exception === 'function' ? self.exception(reject, exception) : (reject(exception), true);
         try {
           // Scenario Runner
           let overrideToString = (func, ctor) => { func.toString = () => ctor.toString(); return func; };
@@ -484,7 +485,7 @@ class Suite {
             }
             catch (e) {
               // catch exceptions within suite callback but outside of test callbacks
-              reject(exception = e); // reject the promise for run()
+              exception = checkExceptionHandler(reject, e);
             }
 
             (typeof suiteTeardown === 'function' ? suiteTeardown : after)(async function () {
@@ -498,7 +499,7 @@ class Suite {
         }
         catch (e) {
           // catch exceptions outside of suite() callback
-          reject(exception = e); // reject the promise for run()
+          exception = checkExceptionHandler(reject, e);
         }
       });
     }
